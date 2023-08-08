ced78d7630d1d806475c620cc34745da45e4f81d
Jason Liu <63383937+jasonIiu@users.noreply.github.com>
2023-08-08 10:39:55 +0800

    Delete ua.js

--- ua.js
+++
@@ -1,6 +0,0 @@
-let header = $request.headers
-let ua = header['User-Agent']
-if (!ua.includes('iPhone')) {
-  header['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41'
-}
-$done({ headers: header })
